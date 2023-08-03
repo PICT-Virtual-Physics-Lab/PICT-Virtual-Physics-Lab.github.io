a4b9fd7b7efb64784e72ab9d26d1d7f2b709ec9e
LUCIFER_001 <101915135+chanekarayush@users.noreply.github.com>
2023-08-03 20:41:23 +0530

    Simulation-Update
    Added Ultrasound Simulator, changed behaviour of tabs- Now website will remember the last opened tab.

---
+++ assets/js/Ultrasound-Simul.js
@@ -0,0 +1,67 @@
+// Ultrasound Slider Code        
+function changePos(){
+var d1top = document.getElementById("d1top").value;
+var d2top = document.getElementById("d2top").value;
+var d1left = document.getElementById("d1left").value;
+var d2left = document.getElementById("d2left").value;
+var detector = document.getElementById("detector").value;
+var obheight = document.getElementById("obheight").value;
+var obwidth = document.getElementById("obwidth").value;
+        
+var slidermax = document.getElementById("detector").max = obwidth;
+console.log(detector)
+        
+if(d1left< 0 ||d1top < 0||d2left < 0||d2top < 0 || obheight < 0 || obwidth < 0){
+    alert("Please Input Positive Values Only");
+    d1left = Math.abs(d1left);
+    d1top = Math.abs(d1top);
+    d2left = Math.abs(d2left);
+    d2top = Math.abs(d2top);
+}
+if(d1left > 50|| d2left > 65){
+    alert("Value Out of Bounds! \n Please input a number from 1 to 70")
+    d1left -= 50;
+    d2left -= 65;
+}
+if(d1top > 20|| d2top > 15){
+    alert("Value Out of Bounds! \n Please input a number from 1 to 70")
+            d1top -= 20;
+            d2top -= 15;
+        }
+if((detector > parseInt(d2left) && detector < (parseInt(d2left)+8)) || 
+((detector > parseInt(d1left) && detector < (parseInt(d1left) + 10)))){
+ // object found
+    document.documentElement.style.setProperty("--graph","--graph1");
+    alert("Object Found!!")
+}
+else{
+ //object not found
+    document.documentElement.style.setProperty("--graph","--graph0");
+}
+    d1top +="vw";
+    d2top +="vw";
+    d1left +="vw";
+    d2left +="vw";
+    obwidth += "vw";
+    obheight += "vw";
+    document.documentElement.style.setProperty("--d1top",d1top);
+    document.documentElement.style.setProperty("--d1left",d1left);
+    document.documentElement.style.setProperty("--d2top",d2top);
+    document.documentElement.style.setProperty("--d2left",d2left);
+    document.documentElement.style.setProperty("--obheight",obheight);
+    ocument.documentElement.style.setProperty("--obwidth",obwidth);
+}
+
+
+// Pop Up Dialog Box Code 
+    myButton.addEventListener("click", function () {
+    myPopup.classList.add("show");
+});
+closePopup.addEventListener("click", function () {
+    myPopup.classList.remove("show");
+});
+window.addEventListener("click", function (event) {
+    if (event.target == myPopup) {
+        myPopup.classList.remove("show");
+    }
+});
--- assets/js/experiment-tabs.js
+++ assets/js/experiment-tabs.js
@@ -1,12 +1,14 @@
 var tabButtons=document.querySelectorAll(".tabContainer .buttonContainer button");
 var tabPanels=document.querySelectorAll(".tabContainer  .tabPanel");
+let panels = localStorage.getItem("panels");
 
-function showPanel(panelIndex,colorCode) {
+localStorage.setItem("panels", 0)
+function showPanel(panelIndex) {
+    localStorage.setItem("panels", panelIndex)
     tabButtons.forEach(function(node){
         node.style.backgroundColor="";
         node.style.color="";
     });
-    tabButtons[panelIndex].style.backgroundColor=colorCode;
     tabButtons[panelIndex].style.color="white";
     tabPanels.forEach(function(node){
         node.style.display="none";
@@ -14,4 +16,4 @@ function showPanel(panelIndex,colorCode) {
     tabPanels[panelIndex].style.display="block";
     tabPanels[panelIndex].style.backgroundColor=colorCode;
 }
-showPanel(0);
+showPanel(panels);
