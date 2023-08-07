53b59620dea595b2475b304023bc351d57db6222
LUCIFER_001 <101915135+chanekarayush@users.noreply.github.com>
2023-08-07 16:42:46 +0530

    Ultrasound-patch 1.0.4
    *Fixed  popup box design flaws
    *Added sliders for convenience of use
    *Added a separate function to change position.

--- assets/js/Ultrasound-Simul.js
+++ assets/js/Ultrasound-Simul.js
@@ -1,45 +1,34 @@
 // Ultrasound Slider Code        
 function changePos(){
-var d1top = document.getElementById("d1top").value;
-var d2top = document.getElementById("d2top").value;
-var d1left = document.getElementById("d1left").value;
-var d2left = document.getElementById("d2left").value;
-var detector = document.getElementById("detector").value;
-var obheight = document.getElementById("obheight").value;
-var obwidth = document.getElementById("obwidth").value;
+    var d1top = document.getElementById("d1top").value;
+    var d2top = document.getElementById("d2top").value;
+    var d1left = document.getElementById("d1left").value;
+    var d2left = document.getElementById("d2left").value;
+    var detector = document.getElementById("detector").value;
+    var obheight = document.getElementById("obheight").value;
+    var obwidth = document.getElementById("obwidth").value;
+            
+    var slidermax = document.getElementById("detector").max = obwidth;
+    d1max = document.getElementById("d1left").max = (obwidth-10);
+    d2max = document.getElementById("d1left").max = (obwidth-8);
         
-var slidermax = document.getElementById("detector").max = obwidth;
-console.log(detector)
-        
-if(d1left< 0 ||d1top < 0||d2left < 0||d2top < 0 || obheight < 0 || obwidth < 0){
-    alert("Please Input Positive Values Only");
-    d1left = Math.abs(d1left);
-    d1top = Math.abs(d1top);
-    d2left = Math.abs(d2left);
-    d2top = Math.abs(d2top);
-}
-if(d1left > 50|| d2left > 65){
-    alert("Value Out of Bounds! \n Please input a number from 1 to 70")
-    d1left -= 50;
-    d2left -= 65;
-}
-if(d1top > 20|| d2top > 15){
-    alert("Value Out of Bounds! \n Please input a number from 1 to 70")
-            d1top -= 20;
-            d2top -= 15;
-        }
-if((detector > parseInt(d2left) && detector < (parseInt(d2left)+8)) || 
-((detector > parseInt(d1left) && detector < (parseInt(d1left) + 10)))){
- // object found
-    document.documentElement.style.setProperty("--graph","--graph1");
-    document.documentElement.style.setProperty("--graph2","--graph0");
-    alert("Object Found!!")
-}
-else{
- //object not found
-    document.documentElement.style.setProperty("--graph","--graph0");
-    document.documentElement.style.setProperty("--graph2","--graph1");
-}
+    if(d1left< 0 ||d1top < 0||d2left < 0||d2top < 0 || obheight < 0 || obwidth < 0){
+        alert("Please Input Positive Values Only");
+        d1left = Math.abs(d1left);
+        d1top = Math.abs(d1top);
+        d2left = Math.abs(d2left);
+        d2top = Math.abs(d2top);
+    }
+    if(d1left > d1max|| d2left > d2max){
+        alert("Value Out of Bounds! \n Please input a number from 0 to " + obwidth)
+        d1left -= d1max;
+        d2left -= d2max;
+    }
+    if(d1top > obheight|| d2top > (obheight-5)){
+        window.alert("Value Out of Bounds! \n Please input a number from 0 to "+ obheight)
+                d1top -= 20;
+                d2top -= 15;
+            }
     d1top +="vw";
     d2top +="vw";
     d1left +="vw";
@@ -51,7 +40,34 @@ else{
     document.documentElement.style.setProperty("--d2top",d2top);
     document.documentElement.style.setProperty("--d2left",d2left);
     document.documentElement.style.setProperty("--obheight",obheight);
-    ocument.documentElement.style.setProperty("--obwidth",obwidth);
+    document.documentElement.style.setProperty("--obwidth",obwidth);
+}
+
+function objsrc(){
+    var d1top = document.getElementById("d1top").value;
+    var d2top = document.getElementById("d2top").value;
+    var d1left = document.getElementById("d1left").value;
+    var d2left = document.getElementById("d2left").value;
+    var detector = document.getElementById("detector").value;
+    var obheight = document.getElementById("obheight").value;
+    var obwidth = document.getElementById("obwidth").value;
+            
+    var slidermax = document.getElementById("detector").max = obwidth;
+    d1max = document.getElementById("d1left").max = (obwidth-10);
+    d2max = document.getElementById("d1left").max = (obwidth-8);
+
+    if((detector > parseInt(d2left) && detector < (parseInt(d2left)+8)) || 
+    ((detector > parseInt(d1left) && detector < (parseInt(d1left) + 10)))){
+    // object found
+        document.documentElement.style.setProperty("--graph1","block");
+        document.documentElement.style.setProperty("--graph0","none");
+        alert("Object Found!!")
+    }
+    else{
+    //object not found
+        document.documentElement.style.setProperty("--graph1","none");
+        document.documentElement.style.setProperty("--graph0","block");
+    }
 }
 
 
